26e87a49cd8c216110b7a54c4064c3b6bac7eb9d
MFatahurRahman <165864583+MFatahurRahman@users.noreply.github.com>
2024-04-29 23:34:07 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>To Do List</h1>
+    <task-list :tasks="tasks" />
+    <input type="text" v-model="newTask" placeholder="Tambahkan kegiatan" />
+    <button @click="addTask">Tambah</button>
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  },
+  data() {
+    return {
+      tasks: [
+        { id: 1, name: 'Belajar Vue.js' },
+        { id: 2, name: 'Menulis kode' },
+        { id: 3, name: 'Membuat proyek baru' }
+      ],
+      newTask: ''
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push({ id: this.tasks.length + 1, name: this.newTask });
+        this.newTask = '';
+      }
+    }
+  }
+};
+</script>
